61ecb84f02d739954901f01b1eb541bff3dcdfed
Akshat Gupta <144452203+Akshat3144@users.noreply.github.com>
2024-01-14 17:11:55 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,7 +42,7 @@ $(document).ready(function () {
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["A Computer Enthusiast", "A Web Developer", "An App Developer","An AI Enthusiast","A Community Service Volunteer],
+    strings: ["A Computer Enthusiast", "A Web Developer", "An App Developer","An AI Enthusiast","A Community Service Volunteer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
